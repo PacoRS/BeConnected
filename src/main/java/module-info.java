ace49a421c24515b5fd4ae529223462d2aedb406
PacoRS <paquiitoor.s@gmail.com>
2022-09-30 10:27:27 +0200

    Proyecto desde cero

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module ramirez.francisco.cosano.sergio.BeConnected {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens ramirez.francisco.cosano.sergio.BeConnected to javafx.fxml;
+    exports ramirez.francisco.cosano.sergio.BeConnected;
+}
