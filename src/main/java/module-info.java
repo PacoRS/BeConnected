b76c6e43e1a4b4aa488154e3616e3d248cbc18cb
PacoRS <paquiitoor.s@gmail.com>
2022-10-04 09:57:15 +0200

    proyecto sin fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ramirez.francisco.cosano.sergio.BeConnected {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
     opens ramirez.francisco.cosano.sergio.BeConnected to javafx.fxml;
     exports ramirez.francisco.cosano.sergio.BeConnected;
--- src/main/java/ramirez/francisco/cosano/sergio/BeConnected/PrimaryController.java
+++ src/main/java/ramirez/francisco/cosano/sergio/BeConnected/PrimaryController.java
@@ -2,11 +2,22 @@ package ramirez.francisco.cosano.sergio.BeConnected;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 public class PrimaryController {
+	
+	
+	@FXML 
+	private ImageView photo;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+        //Image image = new Image("C:/Users/paco1/eclipse-workspace3/BeConnected/src/main/java/Imagen/a1.png");
+		//photo.setImage(image);
+		
     }
 }
