9074e5f3f8adee26dcd36952d28fbb62e493987b
Sercos10 <sergiocosano98@gmail.com>
2022-10-05 12:51:27 +0200

    arreglado module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,8 @@
 module ramirez.francisco.cosano.sergio.BeConnected {
     requires javafx.controls;
     requires javafx.fxml;
-<<<<<<< Vistas
 	requires javafx.graphics;
-=======
     requires java.xml.bind;
->>>>>>> master
 
     opens ramirez.francisco.cosano.sergio.BeConnected to javafx.fxml;
     exports ramirez.francisco.cosano.sergio.BeConnected;
