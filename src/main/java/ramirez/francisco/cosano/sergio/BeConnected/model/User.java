5d2ad00d34c9514451cf71a4c80b9310599d5935
Sercos10 <sergiocosano98@gmail.com>
2022-10-06 14:19:36 +0200

    arreglado User

--- src/main/java/ramirez/francisco/cosano/sergio/BeConnected/model/User.java
+++ src/main/java/ramirez/francisco/cosano/sergio/BeConnected/model/User.java
@@ -12,41 +12,6 @@ import java.io.Serializable;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class User implements IUser, Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private String nickname;
-	private Integer ID;
-
-	public User() {
-		this.nickname = "";
-		this.ID = 0;
-	}
-
-	public User(String nickname, Integer ID) {
-		this.nickname = nickname;
-		this.ID = ID;
-	}
-
-	public String getNickname() {
-		return nickname;
-	}
-
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-
-	public Integer getID() {
-		return ID;
-	}
-
-	public void setID(Integer ID) {
-		this.ID = ID;
-	}
-
-	@Override
-	public String toString() {
-		return "User{" + "nickname='" + nickname + '\'' + ", ID=" + ID + '}';
-	}
-
     private static final long serialVersionUID = 1L;
     private String nickname;
     private Integer ID;
@@ -56,9 +21,9 @@ public class User implements IUser, Serializable {
         this.ID = 0;
     }
 
-    public User(String nickname) {
+    public User(String nickname, Integer ID) {
         this.nickname = nickname;
-        this.ID = (int) (Math.random() * 1000 + 1);
+        this.ID = ID;
     }
 
     public String getNickname() {
@@ -78,20 +43,7 @@ public class User implements IUser, Serializable {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        } else if (obj instanceof User) {
-            User user = (User) obj;
-            return this.nickname.equals(user.nickname) && this.ID.equals(user.ID);
-        } else {
-            return false;
-        }
-    }
-
-    @Override
     public String toString() {
         return "User{" + "nickname='" + nickname + '\'' + ", ID=" + ID + '}';
     }
-
 }
