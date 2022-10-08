42c6b5473bcd581604012c34601a970e93099447
PacoRS <paquiitoor.s@gmail.com>
2022-10-08 10:30:24 +0200

    Terminado

--- src/main/java/ramirez/francisco/cosano/sergio/BeConnected/PrimaryController.java
+++ src/main/java/ramirez/francisco/cosano/sergio/BeConnected/PrimaryController.java
@@ -1,63 +1,55 @@
 package ramirez.francisco.cosano.sergio.BeConnected;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import ramirez.francisco.cosano.sergio.BeConnected.model.RepoSala;
 import ramirez.francisco.cosano.sergio.BeConnected.model.Sala;
 import ramirez.francisco.cosano.sergio.BeConnected.model.User;
 
 public class PrimaryController {
 
-    private static User globalUser;
-	@FXML 
+	private static User globalUser;
+	@FXML
 	private ImageView photo;
-    @FXML
-    private TextField name;
-    @FXML
-    private Button button;
+	@FXML
+	private TextField name;
+	@FXML
+	private Button button;
 
+	@FXML
+	private void switchToSecondary() throws IOException {
+		String nickname = name.getText();
+		boolean flag = false;
+		if (!nickname.equals("")) {
+			globalUser = new User(nickname);
+			do {
+				if (compareUsers(globalUser)) {
+					App.setRoot("secondary");
+				} else {
+					Alert alert = new Alert(Alert.AlertType.ERROR);
+					alert.setTitle("Error");
+					alert.setHeaderText("Error");
+					alert.setContentText("El nickname ya existe, introduzca otra por favor");
+				}
+			} while (flag);
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        String nickname= name.getText();
-        boolean flag = false;
-        if(!nickname.equals("")){
-            globalUser = new User(nickname);
-            do {
-                if (compareUsers(globalUser)){
-                    App.setRoot("secondary");
-                }else{
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error");
-                    alert.setHeaderText("Error");
-                    alert.setContentText("El nickname ya existe, introduzca otra por favor");
-                }
-            }while(flag);
+		} else {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText("Error");
+			alert.setContentText("El nickname no puede estar vacio");
+		}
+	}
 
-        }else{
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Error");
-            alert.setContentText("El nickname no puede estar vacio");
-        }
-    }
+	private boolean compareUsers(User user) {
+		return false;
+		
 
-    private boolean compareUsers(User user1) {
-        /**
-         * Unmarshaler del archivo XML
-         * traigo el array de usuarios de la clase sala
-         * y comparo el usuario nuevo creado con los usuarios
-         * si el usuario ya existe, no se puede crear
-         * ArrayList<User> users = Xml.unmarshal();
-         * for (User user:users){
-         *    if(user1.equals(user)){
-         *    return false;
-         *    }
-         */
-
-        return false;
-    }
+	}
 }
--- src/main/java/ramirez/francisco/cosano/sergio/BeConnected/model/User.java
+++ src/main/java/ramirez/francisco/cosano/sergio/BeConnected/model/User.java
@@ -2,10 +2,14 @@ package ramirez.francisco.cosano.sergio.BeConnected.model;
 
 import ramirez.francisco.cosano.sergio.BeConnected.Interfaces.IUser;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import java.io.File;
 import java.io.Serializable;
 
 @XmlRootElement(name = "User")
@@ -16,7 +20,14 @@ public class User implements IUser, Serializable {
     private String nickname;
     private Integer ID;
 
-    public User() {
+    
+    
+    public User(String nickname) {
+
+		this.nickname = nickname;
+	}
+
+	public User() {
         this.nickname = "";
         this.ID = 0;
     }
@@ -46,4 +57,35 @@ public class User implements IUser, Serializable {
     public String toString() {
         return "User{" + "nickname='" + nickname + '\'' + ", ID=" + ID + '}';
     }
+    
+    public void saveFile(String url,User user) {
+		JAXBContext contexto;
+
+		try {
+			contexto = JAXBContext.newInstance(Message.class);
+			Marshaller m = contexto.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			m.marshal(user, new File(url));
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/*public void loadFile(String url) {
+		JAXBContext contexto;
+		try {
+			contexto = JAXBContext.newInstance(RepoSala.class);
+			Unmarshaller um=contexto.createUnmarshaller();
+			
+			RepoSala newClub= (RepoSala) um.unmarshal(new File(url));
+			salas=newClub.salas;
+		
+			
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}*/
 }
